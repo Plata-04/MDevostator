db4d85fb7c24625872bc2417596f3f2f48b2e359
Plata-04 <Karaplaton@gmail.com>
2022-07-15 23:47:03 +0300

    3

--- core.py
+++ core.py
@@ -1,10 +1,23 @@
-from ast import keyword
-import requests
-from bs4 import BeautifulSoup
-import re
+import time
 import csv
+import os
+
+data = ['ID', 'Type', 'Keywords (ILJ)', 'Title', 'Url']
 
 
+def str_time(): #название файла м-д-ч-м-с
+    named_tuple = time.localtime() # получить struct_time
+    time_string = time.strftime("%m-%d-%H-%M-%S", named_tuple)
+    return time_string
 
+def csv_writer():
+    global data
+    if not os.path.isdir("result"):
+        os.mkdir("result")
+    with open(f"result\\{str(str_time())}.csv", 'w', newline='', encoding="utf_8") as csvfile:
+        writer = csv.writer(csvfile, quotechar='"', delimiter = ";", dialect='unix')
+        for row in data:
+            writer.writerow(row)
 
-
+csv_writer()
+print(str_time())
--- interf.py
+++ interf.py
@@ -1,6 +1,6 @@
 
 from PyQt5 import QtWidgets, uic, QtGui
-from PyQt5.QtCore import QTimer
+from PyQt5.QtCore import QTimer, QEventLoop
 from PyQt5.QtWidgets import QMessageBox
 import os
 import requests
@@ -11,16 +11,17 @@ import time
 import sys
 from threading import *
 
+prog_bar = 0
+kof = 0
+URLS = []
+sleep = 0
 post_id = ""
 title = ""
 keywords = ""
-URL = "https://gp-19.ru/savina-galina-stepanovna-stati/vyvih-rastyazhenie-i-perenapryazhenie-kapsulno-svyazochnogo-apparata-plechevogo-poyasa/"
+black_list = []
+URL = ""
 data = []
-
-data = [
-            ['ID', 'Type', 'Keywords (ILJ)', 'Title', 'Url'],
-            ['21319', 'post', 'ляля,ляля', 'London', 'https://expluataciya-holodilnika.ru/voprosy-i-otvety-po-ekspluatatsii-holodilnika/kak-perevezti-holodilnik-bez-upakovki-pri-pereezde/'],
-                ]
+#https://gp-19.ru/savina-galina-stepanovna-stati/vyvih-rastyazhenie-i-perenapryazhenie-kapsulno-svyazochnogo-apparata-plechevogo-poyasa/
 
 
 def resource_path(relative_path):
@@ -35,46 +36,112 @@ def resource_path(relative_path):
 
 
 
-# class Ui(QtWidgets.QMainWindow):
-#     def __init__(self):
-#         super(Ui, self).__init__()
-#         uic.loadUi(resource_path('untitled.ui'), self)
-#         self.show()
+class Ui(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Ui, self).__init__()
+        uic.loadUi(resource_path('untitled.ui'), self)
+        self.show()
+        self.start.clicked.connect(self.Start)
+        self.load()
+        print(os.listdir("result"))
 
-#     def str_time(self): #название файла м-д-ч-м-с
-#         named_tuple = time.localtime() # получить struct_time
-#         time_string = time.strftime("%m-%d-%H-%M- %S", named_tuple)
-#         return time_string
+    def Start(self):
+        if self.textblack.toPlainText() and self.textURL.toPlainText():
+            global URLS, black_list, sleep, kof, prog_bar
 
-#     def pars(self):
-#         global post_id, title, URL
-#         response = requests.get(URL)
-#         soup = BeautifulSoup(response.text, 'lxml')
-#         title = re.sub(r'[^\w\s]','', soup.select('h1.entry-title')[0].text.strip()).lower()
-#         post_id = re.sub('[^0-9]', '', soup.find("article").get("id"))
-#         print(title, post_id)
+            URLS = self.textURL.toPlainText().split("\n")
+            URLS = list(filter(None, URLS))
+
+            prog_bar = 0
+            kof = 100/len(URLS)
+
+            black_list = self.textblack.toPlainText().split("\n")
+            black_list = list(filter(None, black_list))
+
+            self.textURL.setDisabled(True)
+            self.textblack.setDisabled(True)
+            self.spinBox.setDisabled(True)
+
+            sleep = self.spinBox.value()
+
+            self.save()
+            self.pars()
+            self.csv_writer()
+
+            self.textURL.setDisabled(False)
+            self.textblack.setDisabled(False)
+            self.spinBox.setDisabled(False)   
+
+            print(kof) 
+     
+
+
+    def load(self):
+        if os.path.isfile("blacklist.txt"):
+            global black_list
+            with open("blacklist.txt", "r") as file:
+                for word in file:
+                    word = word.replace("\n", "")
+                    self.textblack.append(word)
+                    black_list.append(word)
+
+    def save (self):
+        global black_list
+        with open("blacklist.txt", "w") as file:
+            for word in black_list:
+                file.write(str(word) + "\n")
         
-#     def black_lister(self):
-#         global keywords, title
-#         title = set(title.split())
-#         b_list = {"как", "термопакетом"}
-#         keywords = ",".join(list(title.difference(b_list)))
 
-#     def csv_data(self):
-#         global post_id, title, keywords, URL, data
-#         data_head = ['ID', 'Type', 'Keywords (ILJ)', 'Title', 'Url']
-#         data_pre = [post_id, "post", keywords, title, URL]
-#         if not data:
-#             data.append(data_head)
-#         data.append(data_pre)
+    def str_time(self): #название файла м-д-ч-м-с
+        named_tuple = time.localtime() # получить struct_time
+        time_string = time.strftime("%m-%d-%H-%M- %S", named_tuple)
+        return time_string  
+
+    def pars(self):
+        global post_id, title, URL, URLS, sleep, kof, prog_bar
+        for url in URLS:
+            URL = url
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, 'lxml')
+            title = re.sub(r'[^\w\s]','', soup.select('h1.entry-title')[0].text.strip()).lower()
+            post_id = re.sub('[^0-9]', '', soup.find("article").get("id"))
+            self.black_lister()
+            self.csv_data()
+            print(url)
+            prog_bar = prog_bar + kof
+            self.progressBar.setValue(int(prog_bar))
+            if sleep:
+                loop = QEventLoop()
+                QTimer.singleShot(sleep, loop.quit)
+                loop.exec_()
+            
+
+            
+
+        
+    def black_lister(self):
+        global keywords, title, black_list
+        title1 = set(title.split())
+        b_list = set(black_list)
+        keywords = ",".join(list(title1.difference(b_list)))
+
+    def csv_data(self):
+        global post_id, title, keywords, URL, data
+        data_head = ['ID', 'Type', 'Keywords (ILJ)', 'Title', 'Url']
+        data_pre = [post_id, "post", keywords, title, URL]
+        if not data:
+            data.append(data_head)
+        data.append(data_pre)
 
 
-#     def csv_writer(self):
-#         global data
-#         with open('test.csv', 'w', newline='', encoding="utf_8") as csvfile:
-#             writer = csv.writer(csvfile, quotechar='"', delimiter = ";", dialect='unix')
-#             for row in data:
-#                 writer.writerow(row)
+    def csv_writer(self):
+        global data
+        if not os.path.isdir("result"):
+            os.mkdir("result")
+        with open(f"result\\{str(self.str_time())}.csv", 'w', newline='', encoding="utf_8") as csvfile:
+            writer = csv.writer(csvfile, quotechar='"', delimiter = ";", dialect='unix')
+            for row in data:
+                writer.writerow(row)
 
 
 
@@ -83,6 +150,6 @@ def resource_path(relative_path):
 
 
 
-# app = QtWidgets.QApplication(sys.argv)
-# window = Ui()
-# app.exec_()
+app = QtWidgets.QApplication(sys.argv)
+window = Ui()
+app.exec_()
