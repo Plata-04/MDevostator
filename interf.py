972b8f97ec2cd80bb089a88d7b1922a8ff923983
Plata-04 <Karaplaton@gmail.com>
2022-07-16 15:12:55 +0300

    con2

--- interf.py
+++ interf.py
@@ -22,7 +22,47 @@ keywords = ""
 black_list = []
 URL = ""
 data = []
-#https://gp-19.ru/savina-galina-stepanovna-stati/vyvih-rastyazhenie-i-perenapryazhenie-kapsulno-svyazochnogo-apparata-plechevogo-poyasa/
+cl = False
+
+
+def csv_data():
+    global post_id, title, keywords, URL, data
+    data_head = ['ID', 'Type', 'Keywords (ILJ)', 'Title', 'Url']
+    data_pre = [post_id, "post", keywords, title, URL]
+    if not data:
+        data.append(data_head)
+    data.append(data_pre)
+
+def black_lister():
+    global keywords, title, black_list
+    title1 = set(title.split())
+    b_list = set(black_list)
+    keywords = ",".join(list(title1.difference(b_list)))
+
+
+def pars():
+    global post_id, title, URL, URLS, sleep, kof, prog_bar, cl
+    for url in URLS:
+        if cl == True:
+            break
+        try:
+            URL = url
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, 'lxml')
+            title = re.sub(r'[^\w\s]','', soup.select('h1.entry-title')[0].text.strip()).lower()
+            post_id = re.sub('[^0-9]', '', soup.find("article").get("id"))
+            black_lister()
+            csv_data()
+            prog_bar = prog_bar + kof
+            #self.progressBar.setValue(int(prog_bar))
+            if sleep:
+                time.sleep(sleep/1000)
+        except: 
+            prog_bar = prog_bar + kof
+            continue
+
+
+
 
 
 def resource_path(relative_path):
@@ -45,6 +85,28 @@ class Ui(QtWidgets.QMainWindow):
         self.load()
         self.start.clicked.connect(self.Start)
         self.open_.clicked.connect(self.open)
+        self.timer=QTimer()
+        self.timer.timeout.connect(self.info)
+        self.timer.start()
+
+    def closeEvent(self, event):
+            global cl
+            # Переопределить colseEvent
+            reply = QMessageBox.question\
+            (self, 'Вы нажали на крестик',
+                "Вы уверены, что хотите уйти?",
+                QMessageBox.Yes,
+                QMessageBox.No)
+            if reply == QMessageBox.Yes:
+                cl = True
+                event.accept()
+            else:
+                event.ignore()
+
+    def info(self):
+        global prog_bar
+        self.progressBar.setValue(int(prog_bar))
+
 
     def Start(self):
         if self.textURL.toPlainText():
@@ -60,6 +122,7 @@ class Ui(QtWidgets.QMainWindow):
             black_list = self.textblack.toPlainText().split("\n")
             black_list = list(filter(None, black_list))
 
+            self.start.setDisabled(True)
             self.textURL.setDisabled(True)
             self.textblack.setDisabled(True)
             self.spinBox.setDisabled(True)
@@ -67,13 +130,22 @@ class Ui(QtWidgets.QMainWindow):
             sleep = self.spinBox.value()
 
             self.save()
-            self.pars()
-            self.csv_writer()
+            TrPars=Thread(target=pars)
+            TrPars.start()
 
+            while TrPars.is_alive():
+                loop = QEventLoop()
+                QTimer.singleShot(1, loop.quit)
+                loop.exec_()
+
+            self.csv_writer()
+            
+            prog_bar = 100
 
             self.textURL.setDisabled(False)
             self.textblack.setDisabled(False)
             self.spinBox.setDisabled(False)   
+            self.start.setDisabled(False)
 
      
     def open(self):
@@ -97,52 +169,11 @@ class Ui(QtWidgets.QMainWindow):
         with open("blacklist.txt", "w") as file:
             for word in black_list:
                 file.write(str(word) + "\n")
-        
 
     def str_time(self): #название файла м-д-ч-м-с
         named_tuple = time.localtime() # получить struct_time
         time_string = time.strftime("%m-%d-%H-%M-%S", named_tuple)
         return time_string  
-
-    def pars(self):
-        global post_id, title, URL, URLS, sleep, kof, prog_bar
-        for url in URLS:
-            try:
-                URL = url
-                response = requests.get(url)
-                soup = BeautifulSoup(response.text, 'lxml')
-                title = re.sub(r'[^\w\s]','', soup.select('h1.entry-title')[0].text.strip()).lower()
-                post_id = re.sub('[^0-9]', '', soup.find("article").get("id"))
-                self.black_lister()
-                self.csv_data()
-                prog_bar = prog_bar + kof
-                self.progressBar.setValue(int(prog_bar))
-                if sleep:
-                    loop = QEventLoop()
-                    QTimer.singleShot(sleep, loop.quit)
-                    loop.exec_()
-            except: 
-                prog_bar = prog_bar + kof
-                continue
-            
-
-            
-
-        
-    def black_lister(self):
-        global keywords, title, black_list
-        title1 = set(title.split())
-        b_list = set(black_list)
-        keywords = ",".join(list(title1.difference(b_list)))
-
-    def csv_data(self):
-        global post_id, title, keywords, URL, data
-        data_head = ['ID', 'Type', 'Keywords (ILJ)', 'Title', 'Url']
-        data_pre = [post_id, "post", keywords, title, URL]
-        if not data:
-            data.append(data_head)
-        data.append(data_pre)
-
 
     def csv_writer(self):
         global data
